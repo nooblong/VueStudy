aed5e86c1235cf824ec744f307b8988cb2fc4f67
龙打野 <18128966990@163.com>
2021-03-15 16:13:46 +0800

    button active

--- tabbar/src/App.vue
+++ tabbar/src/App.vue
@@ -3,18 +3,22 @@
     <tab-bar>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/仓储仓库.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/仓储仓库1.svg" alt="">
         <div slot="item-text">首页</div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/全领域规模.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/全领域规模1.svg" alt="">
         <div slot="item-text">分类</div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/合同.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/合同1.svg" alt="">
         <div slot="item-text">购物车</div>
       </tab-bar-item>
       <tab-bar-item>
         <img slot="item-icon" src="./assets/img/tabbar/用户.svg" alt="">
+        <img slot="item-icon-active" src="./assets/img/tabbar/用户1.svg" alt="">
         <div slot="item-text">我的</div>
       </tab-bar-item>
     </tab-bar>
--- tabbar/src/components/tabbar/TabBarItem.vue
+++ tabbar/src/components/tabbar/TabBarItem.vue
@@ -1,13 +1,21 @@
 <template>
   <div class="tab-bar-item">
-    <slot name="item-icon"></slot>
-    <slot name="item-text"></slot>
+    <slot v-if="!isActive" name="item-icon"></slot>
+    <slot v-else name="item-icon-active"></slot>
+    <div v-bind:class="{active: isActive}">
+      <slot name="item-text"></slot>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TabBarItem"
+  name: "TabBarItem",
+  data() {
+    return {
+      isActive: false
+    }
+  }
 }
 </script>
 
@@ -28,4 +36,7 @@ export default {
   vertical-align: middle;
 }
 
+.active {
+  color: blue;
+}
 </style>
